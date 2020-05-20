7339d3527e5a896f478b5112af05df0d25609af7
Leticia Figueiredo <leticia.ofigueiredo@gmail.com>
2020-05-20 20:39:59 -0300

    teste 01

--- cotovelo.py
+++ cotovelo.py
@@ -2,21 +2,52 @@
 listax = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
           15, 16, 17, 18, 19, 20, 21, 22]
 
-listay = [29.95420683143152, 14.8637849744363296, 11.1916255663311344,7.2851781127241536,
-          5.3530103381035824, 3.9395581891888312, 3.39342290011999,
-          3.10614461599027, 2.46159373963448, 1.60145653328451,
-          1.69722124709078, 1.21554874382424, 1.10878296664376,
-          0.947023374227217, 0.824195677278788, 0.692890991571094,
-          0.647282391877306, 0.614354438965047, 0.567668257118002,
-          0.492576157734102]
+listay = [2.995420683143152E17, 1.48637849744363296E17,
+          1.11916255663311344E17, 7.2851781127241536E16,
+          5.3530103381035824E16, 3.9395581891888312E16, 3.3934229001199996E16,
+          3.106144615990274E16, 2.4615937396344888E16, 1.60145653328451E16,
+          1.6972212470907838E16, 1.2155487438242432E16, 1.108782966643766E16,
+          9.470233742272172E15, 8.241956772787882E15, 6.92890991571094E15,
+          6.472823918773062E15, 6.143544389650474E15, 5.67668257118002E15,
+          4.925761577341026E15]
 
-print(listax[1]-listax[0])
-print(listay[1]-listay[0])
-# def findk (columnx,columny):
-#     x = columnx
-#     y = columny
-#     percentage = []
-#
-#     for i in x:
-#         if x[1] - x[0] != 1:
-#
+
+def findk(columnx, columny):
+    datas = columnx
+    percentage = []
+    teste = []
+    teste1 = []
+    teste2 = []
+
+
+    if datas[1] - datas[0] == 1:
+        datas = columny
+
+    for i in datas:
+        percentage.append(i*100/datas[0])
+
+    for i in range(len(percentage)):
+        print(f'x = {listax[i]}, % = {percentage[i]}')
+        if i == len(percentage) - 1:
+            continue
+        else:
+            print(percentage[i] - percentage[i + 1])
+
+        if percentage[i] - percentage[i+1] < 1:
+            teste.append(listax[i])
+        if percentage[i+1] - percentage[i] < -2:
+            teste1.append(listax[i+1])
+
+    for k in teste:
+        if k in teste1:
+            teste2.append(k)
+
+
+
+    print(teste)
+    print(teste1)
+    print(teste2)
+
+
+print(findk(listay, listax))
+
